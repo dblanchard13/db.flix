f6c2d8eb32ed78eac2fe603c3084362ff449f5f0
dblanchard13 <davidb.prof@gmail.com>
2016-08-21 23:21:56 -0700

    up to details implementation

---
+++ js/Search.jsx
@@ -0,0 +1,45 @@
+const React = require('react')
+const ShowCard = require('./ShowCard')
+const { object } = React.PropTypes
+
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+
+  propTypes: {
+    route: object
+  },
+
+  handleSearchTermEvent (event) {
+    this.setState({ searchTerm: event.target.value })
+  },
+
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>sVideo</h1>
+          <input
+            className='search-input'
+            type='text'
+            placeholder='Search'
+            value={this.state.searchTerm}
+            onChange={this.handleSearchTermEvent}
+            />
+        </header>
+        <div className='shows'>
+          {this.props.route.shows
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
+
+module.exports = Search
